4592b00b0e64f3371a9ffb01ffc68f0113348b67
richpauly13 <pauly13@sbcglobal.net>
2019-01-13 12:36:22 -0600

    Fixed form checkboxes and radios in chrome

--- projects/easy/src/lib/components/form/form.component.ts
+++ projects/easy/src/lib/components/form/form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector:
-		'ez-form, .checkbox-group, .field-group, .fieldset, .form-field, .form-group, .form-h, .form-label, .form-v, .radio-group',
+		'ez-form, .checkbox-group, .field-group, .fieldset, .form-field, .form-group, .form-group-inline, .form-label, .radio-group',
 	templateUrl: './form.component.html',
 	styleUrls: ['./form.component.scss']
 })
