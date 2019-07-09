ef9e7ac8883f065b0d61e592ad327bcc4463980d
richpauly13 <pauly13@sbcglobal.net>
2019-07-09 08:19:16 -0500

    Updated dependencies

--- projects/easy-docs/e2e/src/app.e2e-spec.ts
+++ projects/easy-docs/e2e/src/app.e2e-spec.ts
@@ -26,7 +26,7 @@ describe('workspace-project App', () => {
 		expect(logs).not.toContain(
 			jasmine.objectContaining({
 				level: logging.Level.SEVERE
-			})
+			} as logging.Entry)
 		);
 	});
 });
