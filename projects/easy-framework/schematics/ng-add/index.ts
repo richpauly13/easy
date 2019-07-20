33674e694ba8786ffbba49cb7427737bf74cb206
richpauly13 <pauly13@sbcglobal.net>
2019-07-20 14:31:14 -0500

    Removed strict null checks

--- projects/easy-framework/schematics/ng-add/index.ts
+++ projects/easy-framework/schematics/ng-add/index.ts
@@ -1,7 +1,7 @@
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-export function ngAdd(_options: any): Rule {
+export default function(_options: any): Rule {
 	return (tree: Tree, _context: SchematicContext): Tree => {
 		_context.addTask(new NodePackageInstallTask());
 
