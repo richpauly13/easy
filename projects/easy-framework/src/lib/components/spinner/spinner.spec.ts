348261ff7686396fdb728d778995410a2e43d105
richpauly13 <pauly13@sbcglobal.net>
2019-08-10 22:09:17 -0500

    fix(sass): Fix components sass

--- projects/easy-framework/src/lib/components/spinner/spinner.spec.ts
+++ projects/easy-framework/src/lib/components/spinner/spinner.spec.ts
@@ -2,6 +2,6 @@ import { Spinner } from './spinner';
 
 describe('Spinner', () => {
   it('should create an instance', () => {
-	expect(new Spinner()).toBeTruthy();
+    expect(new Spinner()).toBeTruthy();
   });
 });
