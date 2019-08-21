8cc171c725dc3462fcfe9b91aa78a9a4ac485bb1
richpauly13 <pauly13@sbcglobal.net>
2019-08-21 18:01:50 -0500

    chore(deps): bump dependencies

--- projects/easy-framework/schematics/ng-add/index.ts
+++ projects/easy-framework/schematics/ng-add/index.ts
@@ -16,7 +16,7 @@ export default function(options: EasySchema): Rule {
 
 function addEasyFrameworkToPackageJson(): Rule {
 	return (tree: Tree, context: SchematicContext): Tree => {
-		const version: string = getLibraryVersion();
+		const version: string | undefined = getLibraryVersion();
 
 		addDependencyToPackageJson(tree, 'easy-framework', `^${version}`);
 
