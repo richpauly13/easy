37379ed9ff216e9da7736304f780980be75b8fe9
richpauly13 <pauly13@sbcglobal.net>
2019-11-29 17:25:05 -0600

    feat(color): add color docs

--- projects/easy-docs/src/app/utilities/color/color.component.spec.ts
+++ projects/easy-docs/src/app/utilities/color/color.component.spec.ts
@@ -3,23 +3,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ColorComponent } from './color.component';
 
 describe('ColorComponent', () => {
-  let component: ColorComponent;
-  let fixture: ComponentFixture<ColorComponent>;
+    let component: ColorComponent;
+    let fixture: ComponentFixture<ColorComponent>;
 
-  beforeEach(async(() => {
-	TestBed.configureTestingModule({
-		declarations: [ ColorComponent ]
-	})
-	.compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ColorComponent
+            ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-	fixture = TestBed.createComponent(ColorComponent);
-	component = fixture.componentInstance;
-	fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ColorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-	expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 });
