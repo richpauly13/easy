4fc1721a1a21d83be74180209277c83c86dfb8b6
richpauly13 <pauly13@sbcglobal.net>
2019-11-29 14:55:47 -0600

    feat(width): add width scss and docs

--- projects/easy-docs/src/app/utilities/utilities.module.ts
+++ projects/easy-docs/src/app/utilities/utilities.module.ts
@@ -1,30 +1,30 @@
 import { NgModule } from '@angular/core';
 
+import { ColorComponent } from './color/color.component';
 import { SharedModule } from '../shared/shared.module';
 import { SpaceComponent } from './space/space.component';
 import { TypographyComponent } from './typography/typography.component';
+import { UtilitiesComponent } from './utilities.component';
 import { UtilitiesRoutingModule } from './utilities-routing.module';
 import { VisibilityComponent } from './visibility/visibility.component';
-import { UtilitiesComponent } from './utilities.component';
-import { ColorComponent } from './color/color.component';
 import { WidthComponent } from './width/width.component';
 
 @NgModule({
 	declarations: [
+		ColorComponent,
 		SpaceComponent,
 		TypographyComponent,
 		UtilitiesComponent,
 		VisibilityComponent,
-		ColorComponent,
 		WidthComponent
 	],
 	exports: [
+		ColorComponent,
 		SpaceComponent,
 		TypographyComponent,
 		UtilitiesComponent,
 		UtilitiesRoutingModule,
 		VisibilityComponent,
-		ColorComponent,
 		WidthComponent
 	],
 	imports: [
