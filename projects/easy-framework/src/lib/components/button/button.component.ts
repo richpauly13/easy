fbbcfb1152b1f29b070859480f3557661c5e784d
Paul Chehak <8948337+richpauly13@users.noreply.github.com>
2021-02-21 12:24:48 -0600

    fix(button): add proper disabled state for button

--- projects/easy-framework/src/lib/components/button/button.component.ts
+++ projects/easy-framework/src/lib/components/button/button.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, HostBinding, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-	selector: 'ez-button, .btn-xs, .btn-sm, .btn-md, .btn-lg, .btn-xl, .btn-full',
+	selector: 'ez-button, .btn, .btn-full, .btn-lg, .btn-md, .btn-sm, .btn-xl, .btn-xs',
 	templateUrl: './button.component.html',
 	styleUrls: [
 		'./button.component.scss'
@@ -10,6 +10,7 @@ import { ChangeDetectionStrategy, Component, HostBinding, OnInit, ViewEncapsulat
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent implements OnInit {
+	@HostBinding('attr.disabled') public isHostDisabled: string | null;
 	@HostBinding('attr.type')
 	public get type(): string {
 		return this.hostType;
@@ -19,13 +20,26 @@ export class ButtonComponent implements OnInit {
 		this.hostType = type || 'button';
 	}
 
+	@Input()
+	public get disabled(): string | null {
+		return this.isDisabled;
+	}
+
+	public set disabled(disabled: string | null) {
+		this.isDisabled = disabled === '' ? 'true' : null;
+	}
+
 	private hostType: string;
+	private isDisabled: string | null;
 
 	public constructor() {
 		this.hostType = '';
+		this.isDisabled = null;
+		this.isHostDisabled = null;
 	}
 
 	public ngOnInit(): void {
+		this.isHostDisabled = this.isDisabled;
 		this.type = this.hostType;
 	}
 }
