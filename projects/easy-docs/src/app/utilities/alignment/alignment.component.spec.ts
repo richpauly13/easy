f95dff600afa9690ba9f9fc12d9d2da6625feec2
Paul Chehak <8948337+richpauly13@users.noreply.github.com>
2021-02-21 15:04:21 -0600

    feat(alignment): add alignment classes and docs

---
+++ projects/easy-docs/src/app/utilities/alignment/alignment.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlignmentComponent } from './alignment.component';
+
+describe('AlignmentComponent', () => {
+	let component: AlignmentComponent;
+	let fixture: ComponentFixture<AlignmentComponent>;
+
+	beforeEach(async() => {
+		await TestBed.configureTestingModule({
+			declarations: [
+				AlignmentComponent
+			]
+		})
+		.compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AlignmentComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should be created', () => {
+		expect(component).toBeTruthy();
+	});
+});
---
+++ projects/easy-docs/src/app/utilities/alignment/alignment.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+	selector: 'docs-alignment',
+	templateUrl: './alignment.component.html',
+	styleUrls: [
+		'./alignment.component.scss'
+	],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AlignmentComponent implements OnInit {
+	public constructor(private title: Title) { }
+
+	public ngOnInit(): void {
+		this.title.setTitle('Utilities - Alignment - EASY');
+	}
+}
