5b38aab9e1bc6b429062c36c1906757346bb7f57
Paul Chehak <8948337+richpauly13@users.noreply.github.com>
2023-06-29 23:20:12 -0500

    chore(deps): bump deps

--- projects/easy-framework/src/lib/easy-framework.component.ts
+++ projects/easy-framework/src/lib/easy-framework.component.ts
@@ -7,6 +7,4 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EasyComponent {
-
-}
+export class EasyComponent { }
